684376ccd089a49a19fdf2955504e175a7481703
lweyand <nest_of_muzo@hotmail.com>
2019-12-23 00:24:57 +0100

    chore: initial commit

---
+++ templates/tests/apache2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<HTML>
+
+<HEAD>
+    <TITLE><?php echo "Information Bay: PHP {{ item.key }}"; ?> {{ item.key }}</TITLE>
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF">
+
+<H1>Information Bay</H1>
+
+<P>This information bay has not yet been customized.</P>
+
+</BODY>
+</HTML>
