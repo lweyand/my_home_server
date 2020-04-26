4d308d7bcc776945cce325a2f7fa8813edd087f5
lweyand <nest_of_muzo@hotmail.com>
2020-04-26 13:03:48 +0200

    fix(tls): check certbot validation key after dns update

--- templates/certbot/bookmyname-authenticator.sh
+++ templates/certbot/bookmyname-authenticator.sh
@@ -2,6 +2,8 @@
 # https://fr.faqs.bookmyname.com/frfaqs/dyndns
 auth_basic='{{ certbot_api_key }}'
 ttl=500
+wait=90
+max_try=10
 action=''
 
 if [[ 'x' != "x${1}" ]]; then
@@ -19,7 +21,29 @@ if [[ 'x' != "x${1}" ]]; then
     esac
 fi
 
-wget  -q -O - "https://${auth_basic}@www.bookmyname.com/dyndns/?do=${action}&hostname=_acme-challenge.${CERTBOT_DOMAIN}&type=TXT&ttl=${ttl}&value=\"${CERTBOT_VALIDATION}\""
+wget -q -O - "https://${auth_basic}@www.bookmyname.com/dyndns/?do=${action}&hostname=_acme-challenge.${CERTBOT_DOMAIN}&type=TXT&ttl=${ttl}&value=\"${CERTBOT_VALIDATION}\""
+wget_status=$?
 
-# Sleep to make sure the change has time to propagate over to DNS
-sleep ${ttl}
+if [[ ${wget_status} -ne 0 ]]; then
+  exit 2
+fi
+
+if [[ "${action}" == 'add' ]]; then
+  # Check max_try times if dns is updated
+  count=0
+  found=0
+  while (( count<max_try && found==0 )); do
+    check_dns=$(dig _acme-challenge.${CERTBOT_DOMAIN} TXT | grep -A1 "ANSWER SECTION")
+    if [[ 'x' != "x${check_dns}" && "${check_dns}" == *"${CERTBOT_VALIDATION}"* ]]; then
+      found=1
+    else
+      ((count+=1))
+      sleep ${wait}
+    fi
+  done
+  if [[ ${found} -eq 1 ]]; then
+    exit 0
+  else
+    exit 3
+  fi
+fi
